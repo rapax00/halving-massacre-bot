9553b027ae36770af86152256c5d0bee7729743b
Rapax <gasparpelayo44@gmail.com>
2024-03-24 23:41:12 -0300

    New Feature: create thread for the game

--- src/commands/utility/halvingMassacre.js
+++ src/commands/utility/halvingMassacre.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { ChannelType } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -21,8 +22,27 @@ module.exports = {
                 .setRequired(true)
         ),
     async execute(interaction) {
-        await interaction.reply(
-            '# Comenzó el Halving Massacre!\n## Hagan sus apuestas! :money_with_wings: '
-        );
+        const intervalo = interaction.options.getNumber('intervalo');
+        const bloqueInicial = interaction.options.getNumber('bloque-inicial');
+
+        const message = await interaction.reply({
+            content:
+                '# Nuevo Halving Massacre iniciado! <:guevitos:1217916631834169465>' +
+                `\n- :checkered_flag: Bloque inicial: **${bloqueInicial}**` +
+                `\n- :skull_crossbones: Se masacra cada: **${intervalo}** bloques` +
+                '\n\nHagan sus apuestas! :money_with_wings: :point_down:',
+            fetchReply: true
+        });
+
+        try {
+            const thread = await message.startThread({
+                name: 'Halving Massacre ' + bloqueInicial,
+                type: ChannelType.PublicThread
+            });
+
+            await thread.join();
+        } catch (error) {
+            console.error(error);
+        }
     }
 };
