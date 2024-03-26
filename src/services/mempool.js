12c936c9cf5e2695fc928b26404d1e3dbada5e48
Rapax <gasparpelayo44@gmail.com>
2024-03-25 23:31:33 -0300

    Return json object with both values

--- src/services/mempool.js
+++ src/services/mempool.js
@@ -1,31 +1,22 @@
 const mempoolJS = require('@mempool/mempool.js');
 
-async function getLastBlock() {
+async function getLastBlockAndHash() {
     try {
         const { bitcoin } = mempoolJS();
         const latestBlocks = await bitcoin.blocks.getBlocks({
             start_height: undefined,
             end_height: undefined
         });
-        return Number(latestBlocks[0].height);
+
+        const latestBlock = latestBlocks[0];
+        const blockHeight = latestBlock.height;
+        const blockHash = latestBlock.id;
+
+        return { height: blockHeight, hash: blockHash };
     } catch (error) {
         console.error('Error fetching latest block:', error);
         return null;
     }
 }
 
-async function getLastBlockHash() {
-    try {
-        const { bitcoin } = mempoolJS();
-        const latestBlocks = await bitcoin.blocks.getBlocks({
-            start_height: undefined,
-            end_height: undefined
-        });
-        return latestBlocks[0].hash.toString();
-    } catch (error) {
-        console.error('Error fetching latest block:', error);
-        return null;
-    }
-}
-
-module.exports = { getLastBlock, getLastBlockHash };
+module.exports = { getLastBlockAndHash };
